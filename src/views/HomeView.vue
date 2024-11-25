b28e5969fbe0f44c0994bac3188f6ee4f4a1c7a5
djdjz7 <djdjz7@qq.com>
2024-11-25 16:27:21 +0800

    layout: optimize homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,18 +10,16 @@ const entries = ref<ListEntry[]>(data);
   <main w-screen flex="~ col items-center" p-x-8 md:p-x-16 lg:p-x-32 p-t-40 box-border>
     <h1 font-semibold>吃人协会的音乐推荐</h1>
     <span tracking-widest font-semibold text-gray-500>EDITBLE MUSIC</span>
-    <div grid="~ cols-1 gap-y-4 md:cols-2 lg:cols-3" self-stretch m-t-12>
+    <div grid="~ cols-1 gap-y-4 md:cols-2 lg:cols-3 gap-x-4" self-stretch m-t-12 p-b-32>
       <a v-for="entry in entries" :key="entry.vol" flex="~ items-center" :href="entry.href" relative decoration-none
         class="group">
-        <div relative min-w-24 w-24 h-25>
-          <img absolute top-.5 blur-sm group-hover:blur-md h-24 w-24 transition-filter duration-200 :src="entry.cover">
-          <img absolute h-24 w-24 :src="entry.cover">
-        </div>
+        <img h-24 w-24 :src="entry.cover" shadow-lg group-hover:shadow-xl group-hover:translate-y--.5 transition-all
+          duration-200 block>
         <div m-l-8 flex="~ col" text-black>
           <span text-xs font-semibold text-gray-500 tracking-wide>{{ entry.vol }} · {{ entry.date }}</span>
           <span text-xl font-semibold>{{ entry.title }}</span>
-          <span>{{ entry.album }}</span>
-          <span>{{ entry.artist }}</span>
+          <span text-sm text-gray-500>{{ entry.album }}</span>
+          <span text-sm text-gray-500>{{ entry.artist }}</span>
         </div>
       </a>
     </div>
